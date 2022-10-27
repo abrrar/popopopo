536dbc4daf7ab6a6f0f485585cafda70defa9e66
abrar <llabrrar@gmail.com>
2022-10-27 18:53:19 +0300

    Initial Commit

---
+++ popopopo/popopopoApp.swift
@@ -0,0 +1,17 @@
+//
+//  popopopoApp.swift
+//  popopopo
+//
+//  Created by ABRAR on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct popopopoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
