3e28515b45f30d351ad662cb9aa02712d4d7d424
abrar <llabrrar@gmail.com>
2022-10-27 18:57:24 +0300

    hhhh

--- popopopo/ContentView.swift
+++ popopopo/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, worlfffd!")
+            Text("Hello,!")
         }
         .padding()
     }
